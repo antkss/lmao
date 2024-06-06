8ad40d06e4c5ae1dd647d85788c07a0b22274333
antkss <anfixgapps@gmail.com>
2024-06-06 09:02:15 +0700

    update

--- a.sh
+++ a.sh
@@ -5,5 +5,5 @@ wget "https://raw.githubusercontent.com/antkss/dots-hypr/master/pacman.conf";
 wget "https://raw.githubusercontent.com/antkss/dots-hypr/master/source.sh";
 bash source.sh;
 pacman -Syuu xfce4 --noconfirm;
-pacman -S  chrome-remote-desktop hostname --noconfirm;
+pacman -S yay chrome-remote-desktop hostname --noconfirm;
 # yes 111111 | DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AdLIrYfWb8r1pJBwiEjfvWX3qnRDQ56UZzT_dMrx1u6PkkU3Z77WCpU9RFaxkvSLyAxGxw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname)
