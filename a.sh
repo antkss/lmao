956066a417844eca28438e52813045789481c220
antkss <anfixgapps@gmail.com>
2024-06-06 22:14:30 +0700

    update

--- a.sh
+++ a.sh
@@ -1,3 +1,5 @@
+mkdir /glibc
+cd /glibc
 pacman -Syuu wget gcc base-devel python --noconfirm
 wget "https://ftp.gnu.org/gnu/glibc/glibc-2.39.tar.xz"
 tar -xf glibc-2.39.tar.xz
@@ -6,10 +8,10 @@ mkdir build && cd build
 ../configure --disable-sanity-checks
 make
 cd ..
-tar -cf build.xz build
+tar -cf glibc2.39.xz build
 mkdir /root/.gdrive
 wget "https://raw.githubusercontent.com/antkss/.gdrive/master/lmaobrub/token_v2.json" -O /root/.gdrive/token_v2.json
 wget "https://raw.githubusercontent.com/antkss/.gdrive/master/gdrive" -O /bin/gdrive
 chmod 777 /bin/gdrive
-gdrive upload build.xz -c /root/.gdrive
+gdrive upload glibc2.39.xz -c /root/.gdrive
 
