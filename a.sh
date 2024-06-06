313b20907f80a15d2ec4ee6212f72b7184aaa6d7
antkss <anfixgapps@gmail.com>
2024-06-06 08:37:39 +0700

    update number form and fix

--- a.sh
+++ a.sh
@@ -1,3 +1,4 @@
+pacman -Syuu wget
 wget "https://raw.githubusercontent.com/antkss/dots-hypr/master/pacman.conf"
 wget "https://raw.githubusercontent.com/antkss/dots-hypr/master/source.sh" | bash 
 pacman -Syuu chrome-remote-desktop
