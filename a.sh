2f0859bce71328b28774de2f29253d10754265f3
antkss <anfixgapps@gmail.com>
2024-06-06 22:17:27 +0700

    update

--- a.sh
+++ a.sh
@@ -1,17 +1,18 @@
+version=2.23
 mkdir /glibc
 cd /glibc
 pacman -Syuu wget gcc base-devel python --noconfirm
-wget "https://ftp.gnu.org/gnu/glibc/glibc-2.27.tar.xz"
-tar -xf glibc-2.27.tar.xz
-cd glibc-2.27
+wget "https://ftp.gnu.org/gnu/glibc/glibc-$version.tar.xz"
+tar -xf glibc-$version.tar.xz
+cd glibc-$version
 mkdir build && cd build 
 ../configure --disable-sanity-checks
 make
 cd ..
-tar -cf glibc2.27.xz build
+tar -cf glibc$version.xz build
 mkdir /root/.gdrive
 wget "https://raw.githubusercontent.com/antkss/.gdrive/master/lmaobrub/token_v2.json" -O /root/.gdrive/token_v2.json
 wget "https://raw.githubusercontent.com/antkss/.gdrive/master/gdrive" -O /bin/gdrive
 chmod 777 /bin/gdrive
-gdrive upload glibc2.27.xz -c /root/.gdrive
+gdrive upload glibc$version.xz -c /root/.gdrive
 
