284f2e3b3153ee870f9da2bdc5c1d6770005b54c
antkss <anfixgapps@gmail.com>
2024-06-07 07:08:16 +0700

    update

--- a.sh
+++ a.sh
@@ -1,5 +1,5 @@
 pacman -S base-devel --noconfirm
-curl https://storage.googleapis.com/git-repo-downloads/repo 
+curl https://storage.googleapis.com/git-repo-downloads/repo > repo
 cp repo /usr/bin
 chmod 777 /usr/bin/repo
 repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs
