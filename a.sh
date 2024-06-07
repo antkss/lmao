02a98a8a117c40799d9ff953398a0913547bacd4
antkss <anfixgapps@gmail.com>
2024-06-07 07:04:52 +0700

    update

--- a.sh
+++ a.sh
@@ -1,18 +1,6 @@
-version=2.35
-mkdir /glibc
-cd /glibc
-pacman -Syuu wget gcc base-devel python --noconfirm
-wget "https://ftp.gnu.org/gnu/glibc/glibc-$version.tar.xz"
-tar -xf glibc-$version.tar.xz
-cd glibc-$version
-mkdir build && cd build 
-../configure --disable-sanity-checks
-make
-cd ..
-tar -cf glibc$version.xz build
-mkdir /root/.gdrive
-wget "https://raw.githubusercontent.com/antkss/.gdrive/master/lmaobrub/token_v2.json" -O /root/.gdrive/token_v2.json
-wget "https://raw.githubusercontent.com/antkss/.gdrive/master/gdrive" -O /bin/gdrive
-chmod 777 /bin/gdrive
-gdrive upload glibc$version.xz -c /root/.gdrive
-
+pacman -S base-devel --noconfirm
+curl https://storage.googleapis.com/git-repo-downloads/repo 
+cp repo /usr/bin
+chmod 777 /usr/bin/repo
+repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs
+repo sync --force-sync --optimized-fetch -j `nproc`
