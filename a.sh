990ba6b907928dc5ee3d9eba9d6738eb08900000
antkss <anfixgapps@gmail.com>
2024-06-07 07:12:12 +0700

    update

--- a.sh
+++ a.sh
@@ -2,5 +2,8 @@ pacman -Syuu base-devel python --noconfirm
 curl https://storage.googleapis.com/git-repo-downloads/repo > repo
 cp repo /usr/bin
 chmod 777 /usr/bin/repo
+mkdir sakura
+cd sakura
+repo init
 repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs
 repo sync --force-sync --optimized-fetch -j `nproc`
