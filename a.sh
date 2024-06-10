5e4b9b8814fe0f7d11e08f04d82abcabb48e41a0
antkss <anfixgapps@gmail.com>
2024-06-10 07:49:57 +0700

    update

--- a.sh
+++ a.sh
@@ -1,14 +1,17 @@
 # pacman -Syuu net-tools base-devel python git --noconfirm
-# curl https://storage.googleapis.com/git-repo-downloads/repo > repo
-# cp repo /usr/bin
-# chmod 777 /usr/bin/repo
-# mkdir sakura
-# cd sakura
+curl https://storage.googleapis.com/git-repo-downloads/repo > repo
+cp repo /usr/bin
+chmod 777 /usr/bin/repo
+mkdir kernel
+cd kernel
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+repo init -u https://android.googlesource.com/kernel/manifest -b android-gs-raviole-5.10-android12L
+repo sync
+make
 # repo init
 # repo init --depth=1 -u https://github.com/ProjectSakura/android.git -b 14 --git-lfs
 # repo sync --force-sync --optimized-fetch -j `nproc`
 # sudo /usr/bin/sshd
 # ifconfig
-cd ./attachments
-docker compose up 
 
