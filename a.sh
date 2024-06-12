e836a8ae2a871c8786edf871e0f41630b9245861
antkss <anfixgapps@gmail.com>
2024-06-12 17:00:05 +0700

    update

--- a.sh
+++ a.sh
@@ -5,14 +5,15 @@ pacman -Syuu wget gcc base-devel python --noconfirm
 wget "https://ftp.gnu.org/gnu/glibc/glibc-$version.tar.xz"
 tar -xf glibc-$version.tar.xz
 cd glibc-$version
-mkdir build && cd build 
+mkdir $version && cd $version 
 ../configure --disable-sanity-checks --disable-werror
 make --ignore-errors 
 cd ..
-tar -cf glibc$version.xz build
-mkdir /root/.gdrive
-wget "https://raw.githubusercontent.com/antkss/.gdrive/master/lmaobrub/token_v2.json" -O /root/.gdrive/token_v2.json
+tar -cvJf glibc-$version.tar.xz $version
+#upload file
+mkdir ~/.gdrive
+wget "https://raw.githubusercontent.com/antkss/.gdrive/master/lmaobrub/token_v2.json" -O ~/.gdrive/token_v2.json
 wget "https://raw.githubusercontent.com/antkss/.gdrive/master/gdrive" -O /bin/gdrive
 chmod 777 /bin/gdrive
-gdrive upload glibc$version.xz -c /root/.gdrive
+gdrive upload glibc-$version.xz 
 
