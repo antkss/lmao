4eb33c7033881f9a5fcc9ff5cf2e16dac9ab16f1
antkss <anfixgapps@gmail.com>
2024-06-15 17:32:57 +0700

    update

--- a.sh
+++ a.sh
@@ -1,22 +1,14 @@
-version=2.31;
-mkdir /glibc;
-cd /glibc;
-pacman -Syuu wget gcc base-devel python --noconfirm;
-wget "https://ftp.gnu.org/gnu/glibc/glibc-$version.tar.xz";
-tar -xf glibc-$version.tar.xz;
-cd glibc-$version;
-mkdir $version && cd $version;
-../configure --disable-sanity-checks --disable-werror;
-export MAKEFLAGS="-j$(nproc)"
-echo "makeflag configuration: "
-echo $MAKEFLAGS
-make --ignore-errors;
-cd ..;
-tar -cvJf glibc-$version.tar.xz $version
-#upload file
-mkdir ~/.gdrive
-wget "https://raw.githubusercontent.com/antkss/.gdrive/master/lmaobrub/token_v2.json" -O ~/.gdrive/token_v2.json
-wget "https://raw.githubusercontent.com/antkss/.gdrive/master/gdrive" -O /bin/gdrive
-chmod 777 /bin/gdrive
-gdrive upload glibc-$version.tar.xz 
+git clone https://github.com/antkss/packages
+for i in $(find . -name "*pkg.tar*");do
+    mv $i packages
+done
+cd packages
+for i in $(find . -name "antk.*");do
+    rm $i
+done
+repo-add antk.db.tar.gz *pkg.tar*
+mv antk.db.tar.gz antk.db
+git add .
+git commit -m "update packages"
+git push origin master
 
