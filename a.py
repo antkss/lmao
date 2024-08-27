4b6eeb3a0dfcfc1fe1bc8ecda94b5eb1c8a9a4f8
antkss <anfixgapps@gmail.com>
2024-08-27 19:52:25 +0700

    update

--- a.py
+++ a.py
@@ -3,6 +3,9 @@
 # b = b""
 # for i in range(300):
 #     os.system("cd cac && mkdir cac&& cat \"a\" > cac.txt")
+for i in range(5):
+    with open (f"{hex(0x558+i*0x10)}","w") as f:
+        f.write("a"*(0x558+i*0x10))
   
 
 
